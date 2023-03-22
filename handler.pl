604dcfd58b0076b272b50b6d6c35957038727432
EC2 Default User <ec2-user@ip-172-31-34-113.us-east-2.compute.internal>
2023-03-22 04:01:08 +0000

    trigger working and returning the metadata

--- handler.pl
+++ handler.pl
@@ -3,16 +3,18 @@ use warnings;
 use HTTP::Tiny;
 use Image::ExifTool;
 
+$| = 1;
 
 sub handler {
 
     my ($event, $context) = @_;
 
     my $bucket_name = 'agintel-general-image-bucket-01';
-    my $key = 'asdcxeDJI_0492.JPG';
+    #my $asw = 'asdcxeDJI_0492.JPG';
+    my $asw = $event->{Records}[0]{s3}{object}{key};
     my $region = 'us-east-2';
-    my $url = "https://${bucket_name}.s3.${region}.amazonaws.com/${key}";
-
+    my $url = sprintf("https://%s.s3.%s.amazonaws.com/%s", $bucket_name, $region, $asw);
+    
     my $http = HTTP::Tiny->new;
     my $response = $http->get($url);
 
@@ -23,16 +25,15 @@ sub handler {
 
         my $metadata = { metadata => $info };
         my $metadata_string = join("\n", map { "$_ : $metadata->{metadata}->{$_}" } keys %{$metadata->{metadata}});
-        #print $metadata_string . "\n";
+        print $metadata_string . "\n";
         return {
             statusCode => 200,
-            event => $event
+            event => $event,
+            a => $asw
+            
         };
     } else {
         die $response->{status} . ' ' . $response->{reason};
     }
 }
-
-# test the handler function
-handler();
 
