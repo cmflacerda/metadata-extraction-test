acab33ffb340cb61559cc35160888108d0b5df48
EC2 Default User <ec2-user@ip-172-31-34-113.us-east-2.compute.internal>
2023-03-22 19:17:06 +0000

    reading and writing on buckets properly working

--- handler.pl
+++ handler.pl
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 use HTTP::Tiny;
 use Image::ExifTool;
+use File::Basename;
+use JSON;
 
 $| = 1;
 
@@ -10,11 +12,9 @@ sub handler {
     my ($event, $context) = @_;
 
     my $bucket_name = 'agintel-general-image-bucket-01';
-    #my $asw = 'asdcxeDJI_0492.JPG';
     my $asw = $event->{Records}[0]{s3}{object}{key};
     my $region = 'us-east-2';
     my $url = sprintf("https://%s.s3.%s.amazonaws.com/%s", $bucket_name, $region, $asw);
-    
     my $http = HTTP::Tiny->new;
     my $response = $http->get($url);
 
@@ -22,18 +22,36 @@ sub handler {
         my $image_data = $response->{content};
         my $exifTool = Image::ExifTool->new;
         my $info = $exifTool->ImageInfo(\$image_data);
-
         my $metadata = { metadata => $info };
         my $metadata_string = join("\n", map { "$_ : $metadata->{metadata}->{$_}" } keys %{$metadata->{metadata}});
-        print $metadata_string . "\n";
+        
+        #Writing the data on another bucket
+        my ($filename, $directories, $extension) = fileparse($asw, qr/\.[^.]*/);
+        my $new_key = $filename;
+
+        my $upload_url = sprintf("https://%s.s3.%s.amazonaws.com/%s.txt", 'agintel-upload-bucket', $region, $new_key);
+        my $upload_response = $http->put($upload_url, {
+            content => $metadata_string,
+            headers => {
+                'Content-Type' => 'text/plain',
+                'x-amz-acl' => 'public-read'
+            }
+        });
+        
+
+        if ($upload_response->{success}) {
+            print "Metadata uploaded successfully\n";
+        } else {
+            die "Metadata upload failed: " . $upload_response->{status} . " " . $upload_response->{reason};
+        }
+
+
         return {
-            statusCode => 200,
-            event => $event,
-            a => $asw
-            
+            statusCode => 200            
         };
-    } else {
-        die $response->{status} . ' ' . $response->{reason};
-    }
+
+      } else {
+          die $response->{status} . ' ' . $response->{reason};
+      }
 }
 
